20413bcd80f1e1577fd9c34088cd0b5188251915
Rakhymzhan <rako.aimaganbet@gmail.com>
2020-06-09 14:52:39 +0500

    workingVersion

--- object_list.js
+++ object_list.js
@@ -11,7 +11,7 @@ ymaps.ready(function () {
          * Список всех опций доступен в документации.
          * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/Clusterer.xml#constructor-summary
          */
-            clusterer = new ymaps.Clusterer({
+        clusterer = new ymaps.Clusterer({
             /**
              * Через кластеризатор можно указать только стили кластеров,
              * стили для меток нужно назначать каждой метке отдельно.
@@ -22,6 +22,13 @@ ymaps.ready(function () {
              * Ставим true, если хотим кластеризовать только точки с одинаковыми координатами.
              */
             groupByCoordinates: false,
+            clusterIconLayout: 'default#pieChart',
+            // Радиус диаграммы в пикселях.
+            clusterIconPieChartRadius: 25,
+            // Радиус центральной части макета.
+            clusterIconPieChartCoreRadius: 10,
+            // Ширина линий-разделителей секторов и внешней обводки диаграммы.
+            clusterIconPieChartStrokeWidth: 3,
             /**
              * Опции кластеров указываем в кластеризаторе с префиксом "cluster".
              * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/ClusterPlacemark.xml
@@ -30,6 +37,7 @@ ymaps.ready(function () {
             clusterHideIconOnBalloonOpen: false,
             geoObjectHideIconOnBalloonOpen: false
         }),
+        pointsWithColor = [],
         /**
          * Функция возвращает объект, содержащий данные метки.
          * Поле данных clusterCaption будет отображено в списке геообъектов в балуне кластера.
@@ -39,12 +47,16 @@ ymaps.ready(function () {
          * и балуна геообъектов, можно посмотреть в документации.
          * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/GeoObject.xml
          */
-            getPointData = function (index) {
+        getPointData = function (index) {
+            var buf = pointsWithColor[index];
+            var imgPath = buf.img;
+            var title = buf.title;
+            var desc = buf.desc;
             return {
-                balloonContentHeader: '<font size=3><b><a target="_blank" href="https://yandex.ru">Здесь может быть ваша ссылка</a></b></font>',
-                balloonContentBody: '<p>Ваше имя: <input name="login"></p><p>Телефон в формате 2xxx-xxx:  <input></p><p><input type="submit" value="Отправить"></p>',
-                balloonContentFooter: '<font size=1>Информация предоставлена: </font> балуном <strong>метки ' + index + '</strong>',
-                clusterCaption: 'метка <strong>' + index + '</strong>'
+                balloonContentHeader: '<font size=3><b>'+title+'</b></font>',
+                balloonContentBody: '<img src="'+imgPath+'" style="width: 150px;"><br><p>'+desc+'</p>',
+                balloonContentFooter: '',
+                clusterCaption: '<strong>' + title + '</strong>'
             };
         },
         /**
@@ -52,22 +64,121 @@ ymaps.ready(function () {
          * Все опции, которые поддерживают геообъекты, можно посмотреть в документации.
          * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/GeoObject.xml
          */
-            getPointOptions = function () {
+        getPointOptions = function (color) {
             return {
-                preset: 'islands#violetIcon'
+                preset: color
             };
         },
         points = [
-            [55.831903,37.411961], [55.763338,37.565466], [55.763338,37.565466], [55.744522,37.616378], [55.780898,37.642889], [55.793559,37.435983], [55.800584,37.675638], [55.716733,37.589988], [55.775724,37.560840], [55.822144,37.433781], [55.874170,37.669838], [55.716770,37.482338], [55.780850,37.750210], [55.810906,37.654142], [55.865386,37.713329], [55.847121,37.525797], [55.778655,37.710743], [55.623415,37.717934], [55.863193,37.737000], [55.866770,37.760113], [55.698261,37.730838], [55.633800,37.564769], [55.639996,37.539400], [55.690230,37.405853], [55.775970,37.512900], [55.775777,37.442180], [55.811814,37.440448], [55.751841,37.404853], [55.627303,37.728976], [55.816515,37.597163], [55.664352,37.689397], [55.679195,37.600961], [55.673873,37.658425], [55.681006,37.605126], [55.876327,37.431744], [55.843363,37.778445], [55.875445,37.549348], [55.662903,37.702087], [55.746099,37.434113], [55.838660,37.712326], [55.774838,37.415725], [55.871539,37.630223], [55.657037,37.571271], [55.691046,37.711026], [55.803972,37.659610], [55.616448,37.452759], [55.781329,37.442781], [55.844708,37.748870], [55.723123,37.406067], [55.858585,37.484980]
         ],
         geoObjects = [];
-
     /**
      * Данные передаются вторым параметром в конструктор метки, опции - третьим.
      * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/Placemark.xml#constructor-summary
      */
-    for(var i = 0, len = points.length; i < len; i++) {
-        geoObjects[i] = new ymaps.Placemark(points[i], getPointData(i), getPointOptions());
+
+
+
+
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyBWEoixElYjZcJV2dU17Iqk13j0uCXqRd0",
+        authDomain: "interactive-map-99eef.firebaseapp.com",
+        databaseURL: "https://interactive-map-99eef.firebaseio.com",
+        projectId: "interactive-map-99eef",
+        storageBucket: "interactive-map-99eef.appspot.com",
+        messagingSenderId: "100973636436",
+        appId: "1:100973636436:web:76c6b2cf392d2a9a4776c1",
+        measurementId: "G-PFWTVJ1MZ7"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+
+    function generate(str) {
+        return str.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0;
+            return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+    }
+
+    function getData(){
+        var query = firebase.database().ref("Location").orderByKey();
+        var xCor;
+        query.once("value")
+            .then(function (snapshot) {
+                snapshot.forEach(function (childSnapshot) {
+                    // childData will be the actual contents of the child
+                    
+                    xCor = childSnapshot.child("x").val();
+                    yCor = childSnapshot.child("y").val();
+                    cvet = childSnapshot.child("c").val();
+                    var titleVal = childSnapshot.child("title").val();
+                    var imgVal = childSnapshot.child("img").val();
+                    var descVal = childSnapshot.child("desc").val();
+                    var buffLength = pointsWithColor.length;
+                    var buff = xCor + "," + yCor;
+                    pointsWithColor[buffLength] = {
+                        p: buff,
+                        c: cvet,
+                        title: titleVal,
+                        img: imgVal,
+                        desc: descVal
+                    }
+                    buffLength = points.length;
+                    points[buffLength] = [xCor, yCor];
+    
+                    for (var i = 0, len = points.length; i < len; i++) {
+                        var col;
+                        var buffArray = pointsWithColor[i];
+                        col = buffArray.c;
+                        geoObjects[i] = new ymaps.Placemark(points[i], getPointData(i), getPointOptions(col));
+                    }
+        
+                    clusterer.options.set({
+                        gridSize: 80,
+                        clusterDisableClickZoom: true
+                    });
+                    clusterer.removeAll();
+                    clusterer.add(geoObjects);
+                    myMap.geoObjects.removeAll();
+                    myMap.geoObjects.add(clusterer);
+                    myMap.setBounds(clusterer.getBounds(), {
+                        checkZoomRange: true
+                    });
+                });
+            });
+
+    };
+
+    getData();
+
+
+
+
+
+
+
+
+
+    $("#add").click(function () {
+      
+        uniqID = generate('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx');
+        firebase.database().ref("Location/" + uniqID).set({
+            x: $("#x").val(),
+            y: $("#y").val(),
+            c: $("#color").val(),
+            title: $("#title").val(),
+            desc: $("#desc").val(),
+            img: $("#img").val()
+        });
+        getData();
+    });
+    for (var i = 0, len = points.length; i < len; i++) {
+        var col;
+        var buffArray = pointsWithColor[i];
+        col = buffArray.c;
+        geoObjects[i] = new ymaps.Placemark(points[i], getPointData(i), getPointOptions(col));
     }
 
     /**
